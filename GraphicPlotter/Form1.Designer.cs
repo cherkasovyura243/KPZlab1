4c07161aee11eb31558758d83cba7ff9aeabe753
CherkasovYuriy <ipz243_chyuyu@student.ztu.edu.ua>
2025-05-20 12:46:37 +0300

    OOP Lab8

---
+++ GraphicPlotter/Form1.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace GraphicPlotter
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataVisualization.Charting.ChartArea chartArea2 = new System.Windows.Forms.DataVisualization.Charting.ChartArea();
+            System.Windows.Forms.DataVisualization.Charting.Legend legend2 = new System.Windows.Forms.DataVisualization.Charting.Legend();
+            System.Windows.Forms.DataVisualization.Charting.Series series2 = new System.Windows.Forms.DataVisualization.Charting.Series();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.chart1 = new System.Windows.Forms.DataVisualization.Charting.Chart();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.ErrorImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.ErrorImage")));
+            this.pictureBox1.Image = global::GraphicPlotter.Properties.Resources.Знімок_екрана_2025_05_07_154508;
+            this.pictureBox1.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox1.InitialImage")));
+            this.pictureBox1.Location = new System.Drawing.Point(2, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(159, 71);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // chart1
+            // 
+            this.chart1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            chartArea2.Name = "ChartArea1";
+            this.chart1.ChartAreas.Add(chartArea2);
+            legend2.Name = "Legend1";
+            this.chart1.Legends.Add(legend2);
+            this.chart1.Location = new System.Drawing.Point(2, 87);
+            this.chart1.Name = "chart1";
+            series2.ChartArea = "ChartArea1";
+            series2.Legend = "Legend1";
+            series2.Name = "Series1";
+            this.chart1.Series.Add(series2);
+            this.chart1.Size = new System.Drawing.Size(779, 461);
+            this.chart1.TabIndex = 1;
+            this.chart1.Text = "chart1";
+            // 
+            // label1
+            // 
+            this.label1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Narrow", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(567, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(191, 22);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Черкасов Ю. Ю. ІПЗ 24-3(2)";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(782, 553);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.chart1);
+            this.Controls.Add(this.pictureBox1);
+            this.MinimumSize = new System.Drawing.Size(388, 407);
+            this.Name = "Form1";
+            this.Text = "Графік функції y = (cos(10x))² + x";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.chart1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DataVisualization.Charting.Chart chart1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ GraphicPlotter/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.DataVisualization.Charting;
+
+namespace GraphicPlotter
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            SetupChart();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        private void SetupChart()
+        {
+            chart1.Series.Clear();
+            var series = new Series
+            {
+                ChartType = SeriesChartType.Spline,
+                Color = Color.Red,
+                BorderWidth = 2
+            };
+            chart1.Series.Add(series);
+
+            for (double x = -10; x <= 10; x += 0.1)
+            {
+                double y = Math.Pow(Math.Cos(10 * x), 2) + x;
+                chart1.Series[0].Points.AddXY(x, y);
+            }
+
+            chart1.ChartAreas[0].AxisX.Title = "X";
+            chart1.ChartAreas[0].AxisY.Title = "Y";
+            chart1.ChartAreas[0].AxisX.Minimum = -10;
+            chart1.ChartAreas[0].AxisX.Maximum = 10;
+            chart1.ChartAreas[0].AxisY.Minimum = -10;
+            chart1.ChartAreas[0].AxisY.Maximum = 10;
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            DrawManualGraph(e.Graphics);
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            this.Invalidate(); 
+        }
+        private void DrawManualGraph(Graphics g)
+        {
+            g.Clear(Color.White);
+
+            Pen axisPen = new Pen(Color.Black, 1);
+            Pen graphPen = new Pen(Color.Blue, 2);
+            Font font = new Font("Arial", 10);
+            Brush brush = new SolidBrush(Color.Black);
+
+            int width = this.ClientSize.Width - 250; 
+            int height = this.ClientSize.Height;
+            Point center = new Point(width / 2, height / 2);
+
+            g.DrawLine(axisPen, 20, center.Y, width - 20, center.Y); 
+            g.DrawLine(axisPen, center.X, 20, center.X, height - 20); 
+
+            g.DrawString("X", font, brush, width - 30, center.Y - 20);
+            g.DrawString("Y", font, brush, center.X + 10, 10);
+
+            int marks = 10;
+            float xStep = (width - 40) / (2f * marks);
+            float yStep = (height - 40) / (2f * marks);
+
+            for (int i = 1; i <= marks; i++)
+            {
+                int xPos = center.X + (int)(i * xStep);
+                g.DrawLine(axisPen, xPos, center.Y - 5, xPos, center.Y + 5);
+                g.DrawString((i * 2).ToString(), font, brush, xPos - 10, center.Y + 10);
+
+                xPos = center.X - (int)(i * xStep);
+                g.DrawLine(axisPen, xPos, center.Y - 5, xPos, center.Y + 5);
+                g.DrawString((-i * 2).ToString(), font, brush, xPos - 10, center.Y + 10);
+
+                int yPos = center.Y - (int)(i * yStep);
+                g.DrawLine(axisPen, center.X - 5, yPos, center.X + 5, yPos);
+                g.DrawString((i * 2).ToString(), font, brush, center.X + 10, yPos - 10);
+
+                yPos = center.Y + (int)(i * yStep);
+                g.DrawLine(axisPen, center.X - 5, yPos, center.X + 5, yPos);
+                g.DrawString((-i * 2).ToString(), font, brush, center.X + 10, yPos - 10);
+            }
+            PointF[] points = new PointF[1000];
+            for (int i = 0; i < 1000; i++)
+            {
+                float x = -10 + i * 20f / 999;
+                float y = (float)(Math.Pow(Math.Cos(10 * x), 2) + x);
+                points[i] = new PointF(
+                    center.X + x * xStep / 2,
+                    center.Y - y * yStep / 2);
+            }
+            g.DrawCurve(graphPen, points);
+
+
+        }
+    }
+}
